c7fcd7530885ac84412cee3e36fb6ad7e1baf446
Socorro Samano <48115672+mssc30@users.noreply.github.com>
2021-03-21 13:07:58 -0600

    Petición y trazar ruta ok
    Falta modificar los puntos de inicio y fin

--- app/src/main/java/net/mssc/mismapas/MainActivity.java
+++ app/src/main/java/net/mssc/mismapas/MainActivity.java
@@ -2,9 +2,17 @@ package net.mssc.mismapas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,8 +20,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -47,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .title("Marker"));
 
         // Sets the map type to be "hybrid"
-        mapa.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        //mapa.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
         LatLng sydney = new LatLng(-33.852, 151.211);
         mapa.addMarker(new MarkerOptions().
@@ -67,18 +83,68 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         mapa.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
-        Polyline polyline1 = mapa.addPolyline(new PolylineOptions()
-                .clickable(true)
-                .add(
-                        new LatLng(-35.016, 143.321),
-                        new LatLng(-34.747, 145.592),
-                        new LatLng(-34.364, 147.891),
-                        new LatLng(-33.501, 150.217),
-                        new LatLng(-32.306, 149.248),
-                        new LatLng(-32.491, 147.309)));
-        // Store a data object with the polyline, used here to indicate an arbitrary type.
-        polyline1.setTag("A");
+        peticionRutas();
     }
+
+    private void peticionRutas() {
+        //LISTA DE LISTAS DE PUNTOS, ES PARA TRAZAR LA POLILINEA
+        final List<List<LatLng>> listaPoli = new ArrayList<>();
+
+        //STRING DE LA PETICION A LA API DE DIRECCIONES
+        String URL_PETICION = "https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyBHtYD_i3eqYqdCroUTQDwzb5FtqD323oc";
+
+        //REQUESTQUEUE PARA REALIZAR LA PETICION UNA VEZ CONSTRUIDA
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        //CONSTRUCCION DE LA STRING REQUEST
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                URL_PETICION,
+                new Response.Listener<String>() {
+                    @Override
+                    //CUANDO RESPONDA
+                    public void onResponse(String response) {
+                        try {
+                            //OBTENER EL OBJETO DE RESPUESTA
+                            JSONObject obj = new JSONObject(response);
+                            //OBTENER EL ARREGLO DE PASOS DEL RESPONSE
+                            JSONArray listaPuntos = obj.getJSONArray("routes")
+                                    .getJSONObject(0).getJSONArray("legs")
+                                    .getJSONObject(0).getJSONArray("steps");
+
+                            //POR CADA PASO EN EL RESPONSE, OBTENER EL OBJETO POLYLINE Y EL STRING DE SUS PUNTOS CODIFICADO
+                            for (int i = 0; i<listaPuntos.length(); i++){
+                                String puntos = listaPuntos.getJSONObject(i).
+                                        getJSONObject("polyline").
+                                        getString("points");
+
+                                //AGREGAR LA LISTA DE PUNTOS A LA LISTA DE LISTAS.
+                                //SE USA LA API POLYUTIL PARA DECODIFICAR LOS PUNTOS
+                                listaPoli.add(PolyUtil.decode(puntos));
+                            }
+
+                            //POR CADA LISTA DE PUNTOS EN LA LISTA POLI SE DIBUJA UNA POLILINEA EN EL MAPA
+                            //OBTENIENDO LA RUTA COMPLETA
+                            for (int i = 0; i<listaPoli.size(); i++){
+                                Log.d("RUTAS", listaPoli.get(i).toString());
+                                mapa.addPolyline(new PolylineOptions().addAll(listaPoli.get(i)).color(Color.RED));
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //MOSTRAR ERROR SI OCURRE
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        //AGREGAR PETICION A LA COLA
+        requestQueue.add(stringRequest);
+}
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
